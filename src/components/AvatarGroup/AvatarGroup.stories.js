229f9c844e5b7bf641353bc37c1691148fc1f8f0
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-07-28 12:11:06 -0300

    refactor(avatar-group): update spacing when avatar size is changed

--- src/components/AvatarGroup/AvatarGroup.stories.js
+++ src/components/AvatarGroup/AvatarGroup.stories.js
@@ -78,36 +78,108 @@ export const WithSizes = () => ({
 export const WithMoreAvatars = () => ({
   components: { SbAvatarGroup, SbAvatar },
   template: `
-  <SbAvatarGroup size="large">
-    <SbAvatar
-      src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
-    />
+  <div>
+    <div>
+      <SbAvatarGroup size="large">
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
 
-    <SbAvatar name="John Doe" />
+        <SbAvatar name="John Doe" />
 
-    <SbAvatar
-      src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
-    />
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
 
-    <SbAvatar
-      src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
-    />
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
 
-    <SbAvatar
-      src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
-    />
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
 
-    <SbAvatar name="John Doe" />
+        <SbAvatar name="John Doe" />
 
-    <SbAvatar
-      src="https://avatars1.githubusercontent.com/u/7952803?s=400&u=0fd8a3a0721768210fdcedb7607e9ad33af9f7ad&v=4"
-      name="Dominik Angerer"
-    />
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/7952803?s=400&u=0fd8a3a0721768210fdcedb7607e9ad33af9f7ad&v=4"
+          name="Dominik Angerer"
+        />
 
-    <SbAvatar
-      src="https://avatars1.githubusercontent.com/u/160495?s=460&u=b88ece40883d2e9716e833f6a3c78c56ca3eb14f&v=4"
-      name="Alexander Feiglstorfer"
-    />
-  </SbAvatarGroup>
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/160495?s=460&u=b88ece40883d2e9716e833f6a3c78c56ca3eb14f&v=4"
+          name="Alexander Feiglstorfer"
+        />
+      </SbAvatarGroup>
+    </div>
+
+    <div>
+      <SbAvatarGroup>
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar name="John Doe" />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar name="John Doe" />
+
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/7952803?s=400&u=0fd8a3a0721768210fdcedb7607e9ad33af9f7ad&v=4"
+          name="Dominik Angerer"
+        />
+
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/160495?s=460&u=b88ece40883d2e9716e833f6a3c78c56ca3eb14f&v=4"
+          name="Alexander Feiglstorfer"
+        />
+      </SbAvatarGroup>
+    </div>
+
+    <div>
+      <SbAvatarGroup size="small">
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar name="John Doe" />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar
+          src="https://avatars0.githubusercontent.com/u/20342656?s=460&u=1f62c95c10543861ad74b58a3c03cd774e7a4fa4&v=4"
+        />
+
+        <SbAvatar name="John Doe" />
+
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/7952803?s=400&u=0fd8a3a0721768210fdcedb7607e9ad33af9f7ad&v=4"
+          name="Dominik Angerer"
+        />
+
+        <SbAvatar
+          src="https://avatars1.githubusercontent.com/u/160495?s=460&u=b88ece40883d2e9716e833f6a3c78c56ca3eb14f&v=4"
+          name="Alexander Feiglstorfer"
+        />
+      </SbAvatarGroup>
+    </div>
+  </div>
   `
 })
