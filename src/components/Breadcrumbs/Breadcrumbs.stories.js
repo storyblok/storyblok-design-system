9f770aabcf02ece91bf06032d5785fceb4cbb8c1
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-08-28 17:34:17 -0300

    refactor(breadcrumbs): refactor breadcrumb stories

--- src/components/Breadcrumbs/Breadcrumbs.stories.js
+++ src/components/Breadcrumbs/Breadcrumbs.stories.js
@@ -1,13 +1,44 @@
-import { withKnobs, boolean } from '@storybook/addon-knobs'
-
 import { SbBreadcrumbs } from '.'
+
+const Template = args => ({
+  components: { SbBreadcrumbs },
+  props: Object.keys(args),
+  template: `
+    <div style="max-width: 400px">
+      <SbBreadcrumbs v-bind="{ items, isLargeSection }" />
+    </div>
+  `
+})
 
 // default export defines configurations to all stories
 export default {
   title: 'SbBreadcrumbs',
   component: SbBreadcrumbs,
   excludeStories: /.*Data$/,
-  decorators: [withKnobs]
+  parameters: {
+    docs: {
+      description: {
+        component: 'Breadcrumbs show users their current location relative to the information architecture and enable them to quickly move up to a parent level or previous step.'
+      }
+    }
+  },
+  args: {
+    items: [],
+    isLargeSection: false
+  },
+  argTypes: {
+    items: {
+      name: 'items',
+      description: 'Breadcrumb items'
+    },
+    isLargeSection: {
+      name: 'isLargeSection',
+      description: 'Enable show the username on the right',
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
 }
 
 export const defaultBreadcrumbItemsData = [
@@ -42,110 +73,104 @@ export const defaultBreadcrumbItemsData = [
   }
 ]
 
-export const Default = () => ({
-  components: { SbBreadcrumbs },
-  props: {
-    items: {
-      default: () => [
-        ...defaultBreadcrumbItemsData.slice(0, 2),
-        {
-          label: 'CTA Section',
-          isActive: true
-        }
-      ]
+export const Default = Template.bind({})
+
+Default.args = {
+  items: [
+    ...defaultBreadcrumbItemsData.slice(0, 2),
+    {
+      label: 'CTA Section',
+      isActive: true
+    }
+  ]
+}
+
+export const WithLargeSection = Template.bind({})
+
+WithLargeSection.args = {
+  items: [
+    ...defaultBreadcrumbItemsData.slice(0, 2),
+    {
+      label: 'CTA Section',
+      isActive: true
+    }
+  ],
+  isLargeSection: true
+}
+
+WithLargeSection.parameters = {
+  docs: {
+    description: {
+      story: 'When you set the attribute `isLargeSection`, the last item will be show in a new line'
+    }
+  }
+}
+
+export const LongBreadcrums = Template.bind({})
+
+LongBreadcrums.args = {
+  items: [
+    ...defaultBreadcrumbItemsData.slice(0, -1),
+    {
+      label: 'Button CTA',
+      href: '#Button-CTA',
+      title: 'Button CTA'
     },
-    isLargeSection: {
-      default: boolean('Enable Large Section', false)
+    {
+      label: 'CTA Section',
+      isActive: true
     }
-  },
-  template: `
-    <SbBreadcrumbs :items="items" :is-large-section="isLargeSection" />
-  `
-})
+  ]
+}
 
-export const WithLargeSection = () => ({
-  components: { SbBreadcrumbs },
-  props: {
-    items: {
-      default: () => [
-        ...defaultBreadcrumbItemsData.slice(0, 2),
-        {
-          label: 'CTA Section',
-          isActive: true
-        }
-      ]
+LongBreadcrums.parameters = {
+  docs: {
+    description: {
+      story: 'When a path contains more than five levels of entities, it should have a `SbDropdown` with the rest of the items'
     }
-  },
-  template: `
-    <SbBreadcrumbs :items="items" is-large-section />
-  `
-})
+  }
+}
 
-export const LongBreadcrums = () => ({
-  components: { SbBreadcrumbs },
-  props: {
-    items: {
-      default: () => [
-        ...defaultBreadcrumbItemsData.slice(0, -1),
-        {
-          label: 'Button CTA',
-          href: '#Button-CTA',
-          title: 'Button CTA'
-        },
-        {
-          label: 'CTA Section',
-          isActive: true
-        }
-      ]
-    }
-  },
-  template: `
-    <div style="max-width: 400px">
-      <SbBreadcrumbs :items="items" />
-    </div>
-  `
-})
+export const LongBreadcrumsInTwoLines = Template.bind({})
 
-export const LongBreadcrumsInTwoLines = () => ({
-  components: { SbBreadcrumbs },
-  props: {
-    items: {
-      default: () => [
-        ...defaultBreadcrumbItemsData.slice(0, -1),
-        {
-          label: 'CTA Section',
-          isActive: true
-        }
-      ]
+LongBreadcrumsInTwoLines.args = {
+  items: [
+    ...defaultBreadcrumbItemsData.slice(0, -1),
+    {
+      label: 'CTA Section',
+      isActive: true
     }
-  },
-  template: `
-    <div style="max-width: 400px">
-      <SbBreadcrumbs :items="items" />
-    </div>
-  `
-})
+  ]
+}
 
-export const ItemsTruncated = () => ({
-  components: { SbBreadcrumbs },
-  props: {
-    items: {
-      default: () => [
-        ...defaultBreadcrumbItemsData.slice(0, 2),
-        {
-          label: 'Long hero section name tooltip',
-          href: '#test-truncated'
-        },
-        {
-          label: 'CTA Section',
-          isActive: true
-        }
-      ]
+LongBreadcrumsInTwoLines.parameters = {
+  docs: {
+    description: {
+      story: 'The SbBreadcrumbs can be split in two lines'
     }
-  },
-  template: `
-    <div style="max-width: 400px">
-      <SbBreadcrumbs :items="items" />
-    </div>
-  `
-})
+  }
+}
+
+export const ItemsTruncated = Template.bind({})
+
+ItemsTruncated.args = {
+  items: [
+    ...defaultBreadcrumbItemsData.slice(0, 2),
+    {
+      label: 'Long hero section name tooltip',
+      href: '#test-truncated'
+    },
+    {
+      label: 'CTA Section',
+      isActive: true
+    }
+  ]
+}
+
+ItemsTruncated.parameters = {
+  docs: {
+    description: {
+      story: 'When have long label texts, it should truncate them'
+    }
+  }
+}
