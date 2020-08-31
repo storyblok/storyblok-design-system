8d561e1b5892c6b59df793a829221205fe3f73f3
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-08-31 18:29:06 -0300

    feat(tag): create tag component

---
+++ src/components/Tag/Tag.stories.js
@@ -0,0 +1,77 @@
+import SbTag from '.'
+import { tagTypes } from './lib'
+
+const BadgeTemplate = args => ({
+  components: { SbTag },
+  props: Object.keys(args),
+  template: `
+    <SbTag
+      v-bind="{
+        closable,
+        label,
+        type
+      }"
+    />
+  `
+})
+
+export default {
+  title: 'SbTag',
+  component: SbTag,
+  parameters: {
+    docs: {
+      description: {
+        component: ''
+      }
+    }
+  },
+  args: {
+    closable: false,
+    label: 'Tag Label',
+    type: 'primary'
+  },
+  argTypes: {
+    closable: {
+      name: 'closable',
+      description: 'Render a close icon on the right',
+      control: {
+        type: 'boolean'
+      }
+    },
+    label: {
+      name: 'label',
+      description: 'Label to `SbTag`',
+      control: {
+        type: 'text'
+      }
+    },
+    type: {
+      name: 'type',
+      description: 'Type of the `SbTag`',
+      control: {
+        type: 'select',
+        options: tagTypes
+      },
+      type: {
+        required: true
+      }
+    }
+  }
+}
+
+export const Default = BadgeTemplate.bind({})
+
+export const Closable = BadgeTemplate.bind({})
+
+Closable.args = {
+  closable: true,
+  label: 'Closable tag'
+}
+
+Closable.parameters = {
+  docs: {
+    description: {
+      story: 'When you set the `closable` property, it will be render a close icon on the right'
+    }
+  }
+}
---
+++ src/components/Tag/index.js
@@ -0,0 +1,58 @@
+import './tag.scss'
+
+import { includes } from '../../utils'
+
+import SbIcon from '../Icon'
+import { tagTypes } from './lib'
+
+const SbTag = {
+  name: 'SbTag',
+
+  props: {
+    closable: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: null
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: type => includes(tagTypes, type)
+    }
+  },
+
+  render (h) {
+    const tagProps = {
+      staticClass: `sb-tag sb-tag--${this.type}`,
+      class: {
+        'sb-tag--closable': this.closable
+      }
+    }
+
+    const renderClosableIcon = () => {
+      return h(SbIcon, {
+        props: {
+          name: 'close',
+          size: 'small'
+        },
+        on: {
+          click: (event) => {
+            this.$emit('close', event)
+          }
+        }
+      })
+    }
+
+    const children = [
+      this.label || this.$slots.default,
+      this.closable && renderClosableIcon()
+    ]
+
+    return h('div', tagProps, children)
+  }
+}
+
+export default SbTag
