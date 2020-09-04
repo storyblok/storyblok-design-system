877592f8c0a8ed768d21e446b21f660cb0f01ba0
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-09-04 18:19:10 -0300

    feat(tooltip): implement Storybook story and improve accessibility

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,6 +1,22 @@
 import './tooltip.scss'
-import { includes } from '../../utils'
+import { includes, getRandomNumber } from '../../utils'
 import { availablePositions } from './lib'
+
+/**
+ * @method randomString
+ * @param  {Number} length
+ * @return {String}
+ */
+const randomString = length => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(getRandomNumber(0, length))
+  }
+
+  return result
+}
 
 /**
  * SbTooltip component
@@ -10,7 +26,15 @@ import { availablePositions } from './lib'
 export default {
   name: 'SbTooltip',
 
+  data: () => ({
+    isVisibleTooltip: false
+  }),
+
   props: {
+    id: {
+      type: String,
+      default: () => `sb-tooltip-${randomString(5)}`
+    },
     label: {
       type: String
     },
@@ -21,22 +45,53 @@ export default {
     }
   },
 
+  methods: {
+    showTooltip () {
+      this.isVisibleTooltip = true
+    },
+    hideTooltip () {
+      this.isVisibleTooltip = false
+    }
+  },
+
   render (h) {
+    const children = this.$slots.default
+    if (children.length > 1) {
+      return console.error('[SbTooltip]: The SbTooltip component only expects one child.')
+    }
+
+    const { id, label } = this
+
     const tooltipProps = {
-      staticClass: `sb-tooltip sb-tooltip--${this.position}`
+      staticClass: `sb-tooltip sb-tooltip--${this.position}`,
+      attrs: {
+        tabindex: 0,
+        'aria-describedby': id
+      },
+      on: {
+        focus: this.showTooltip,
+        blur: this.hideTooltip,
+        mouseenter: this.showTooltip,
+        mouseleave: this.hideTooltip
+      }
     }
 
     const renderTooltipLabel = () => {
       return h('span', {
-        staticClass: 'sb-tooltip__label'
-      }, this.label)
+        staticClass: 'sb-tooltip__label',
+        attrs: {
+          id,
+          role: 'tooltip',
+          'aria-hidden': !this.isVisibleTooltip + ''
+        }
+      }, label)
     }
 
-    const children = [
+    const content = [
       ...this.$slots.default,
       this.label && renderTooltipLabel()
     ]
 
-    return h('div', tooltipProps, children)
+    return h('div', tooltipProps, content)
   }
 }
