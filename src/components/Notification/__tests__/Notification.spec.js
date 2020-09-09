a860624110426835219d8fab7e931bb67800e3ed
ademarCardoso <ac@storyblok.com>
2020-09-09 17:57:27 -0300

    tests(notification): refactoring all tests

--- src/components/Notification/__tests__/Notification.spec.js
+++ src/components/Notification/__tests__/Notification.spec.js
@@ -10,72 +10,33 @@ const factory = propsData => {
 describe('SbNotification tests', () => {
   it('test default notification', async () => {
     const wrapper = factory({
-      status: 'success',
-      title: 'Notification title',
-      description: 'Subtitle text goes here.',
-      linkNname: 'Example link',
-      link: 'http://#'
+      status: 'positive'
     })
 
-    expect(wrapper.find('div').attributes('class')).toBe('sb-notification sb-notification--success')
+    expect(wrapper.find('div').attributes('class')).toBe('sb-notification sb-notification--positive')
   })
 
-  it('test short notification', async () => {
+  it('test full size notification', async () => {
     const wrapper = factory({
-      status: 'success',
-      title: 'Notification title',
-      linkNname: 'Example link',
-      link: 'http://#',
-      short: true
+      isFull: true
     })
 
-    expect(wrapper.find('div').find('div').attributes('class')).toBe('sb-notification sb-notification--success sb-notification--short sb-notification-banner')
+    expect(wrapper.find('div').attributes('class')).toBe('sb-notification sb-notification--general sb-notification--full')
   })
 
-  it('test banner notification', async () => {
+  it('test notification content', async () => {
     const wrapper = factory({
-      status: 'info',
-      title: 'Notification title',
-      linkNname: 'Example link',
-      link: 'http://#',
-      format: 'banner'
-    })
-
-    expect(wrapper.find('div').attributes('class')).toBe('sb-notification sb-notification--info sb-notification-banner')
-  })
-
-  it('test notification assert', async () => {
-    const wrapper = factory({
-      status: 'info',
+      status: 'positive',
       title: 'Notification title',
       description: 'A awesome description',
       linkName: 'Example link',
       link: 'http://storyblok.com'
     })
 
-    const notification = wrapper.findAll('div')
+    expect(wrapper.find('div').attributes('class')).toBe('sb-notification sb-notification--positive sb-notification--content')
 
-    const classes = ['sb-notification sb-notification--info', 'sb-notification--header', 'sb-notification--content']
+    expect(wrapper.find('span').text()).toBe('Notification title')
 
-    notification.filter((element, index) => {
-      // test if all classes at component exists
-      expect(element.attributes('class')).toBe(classes[index])
-
-      // test title of the notification
-      if (element.find('span').attributes('class') === 'sb-notification--title') {
-        expect(element.find('span').text()).toBe('Notification title')
-      }
-
-      // test text of description
-      if (element.find('p').exists()) {
-        expect(element.find('p').text()).toBe('A awesome description')
-      }
-
-      // test link atributes
-      if (element.find('a').exists()) {
-        expect(element.find('a').text()).toBe('Example link')
-        expect(element.find('a').attributes('href')).toBe('http://storyblok.com')
-      }
-    })
+    expect(wrapper.find('a').text()).toBe('Example link')
   })
 })
