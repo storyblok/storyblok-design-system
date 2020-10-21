312928da154f80b9865d1d9643c25778dcb06778
ademarCardoso <ac@storyblok.com>
2020-10-21 18:02:22 -0300

    style(drop-area): adding description to props in drop-area.stories

--- src/components/DropArea/DropArea.stories.js
+++ src/components/DropArea/DropArea.stories.js
@@ -22,21 +22,21 @@ export default {
   argTypes: {
     accept: {
       name: 'accept',
-      description: '',
+      description: 'Prop `accept` works as a filter, pass the files you want to be added, for example: `:accept="image/png, image/jpeg"`, so only `.png` and `.jpeg`images  will be accepted',
       control: {
         type: 'text'
       }
     },
     maxFileSize: {
       name: 'maxFileSize',
-      description: '',
+      description: 'The `maxFileSize` prop defines the maximum size that each file can have, the prop receives the size in `KB` kylobytes.',
       control: {
         type: 'text'
       }
     },
     maxFile: {
       name: 'maxFile',
-      description: '',
+      description: 'The `maxFile` prop defines the maximum number of files that can be sent at once, that is, at each drop event. Note if the number is greater, no files will be returned.',
       control: {
         type: 'text'
       }
