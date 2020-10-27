420af2cff709699d6b883ed0a46286087278f0c5
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-10-26 23:54:51 -0300

    tests(minibrowser): create tests for `SbMinibrowser` component

---
+++ src/components/Minibrowser/__tests__/Minibrowser.spec.js
@@ -0,0 +1,105 @@
+import { mount } from '@vue/test-utils'
+
+import { waitMs } from '../../../utils/tests-utils'
+import { SbMinibrowser } from '..'
+import { browserOptionsData } from '../Minibrowser.stories.js'
+
+describe('SbMinibrowser component', () => {
+  const itemClass = '.sb-minibrowser__list-item-name'
+
+  it('should perform the correct navigation', async () => {
+    const wrapper = mount(SbMinibrowser, {
+      propsData: {
+        options: [...browserOptionsData]
+      }
+    })
+
+    // PROCESS FOR FIRST LEVEL
+    const firstElement = wrapper.findAll(itemClass).at(0)
+
+    expect(firstElement.text()).toBe('Landing Page')
+
+    await firstElement.trigger('click')
+
+    // getting the first parameter from first execution
+    const result = wrapper.emitted('navigate')[0][0]
+    expect(result.label).toEqual('Landing Page')
+
+    // PROCESS FOR SECOND LEVEL
+    const newFirstElement = wrapper.findAll(itemClass).at(0)
+    expect(newFirstElement.text()).toBe('PPC')
+
+    await newFirstElement.trigger('click')
+
+    // getting the first parameter from second execution
+    const newResult = wrapper.emitted('navigate')[1][0]
+    expect(newResult.label).toBe('PPC')
+
+    // PROCESS FOR THIRD LEVEL
+    // checking if, in the third level, we have the correct item in the first position
+    const thirdElement = wrapper.findAll(itemClass).at(0)
+    expect(thirdElement.text()).toBe('e-commerce')
+  })
+
+  it('should filter the items by using the search input', async () => {
+    const wrapper = mount(SbMinibrowser, {
+      propsData: {
+        options: [...browserOptionsData]
+      }
+    })
+
+    await wrapper
+      .find('input[type="search"]')
+      .setValue('Jobs')
+
+    await wrapper.vm.$nextTick()
+
+    await waitMs(300)
+
+    expect(wrapper.vm.searchInput).toBe('Jobs')
+
+    expect(wrapper.find(itemClass).text()).toBe('Jobs')
+  })
+
+  it('should show a not found message', async () => {
+    const wrapper = mount(SbMinibrowser, {
+      propsData: {
+        options: [...browserOptionsData]
+      }
+    })
+
+    await wrapper
+      .find('input[type="search"]')
+      .setValue('foobar')
+
+    await wrapper.vm.$nextTick()
+
+    expect(
+      wrapper.find('.sb-minibrowser__not-found').text()
+    ).toBe('No matches for "foobar"')
+  })
+
+  it('should clear the input element when clicks on Escape key', async () => {
+    const wrapper = mount(SbMinibrowser, {
+      propsData: {
+        options: [...browserOptionsData]
+      }
+    })
+
+    const searchInput = wrapper.find('input[type="search"]')
+
+    searchInput.setValue('foobar')
+
+    expect(wrapper.vm.searchInput).toBe('foobar')
+
+    searchInput.element.focus()
+
+    await searchInput.trigger('keydown', {
+      key: 'Escape'
+    })
+
+    expect(wrapper.find(itemClass).text()).toBe('Landing Page')
+
+    expect(wrapper.vm.searchInput).toBe('')
+  })
+})
