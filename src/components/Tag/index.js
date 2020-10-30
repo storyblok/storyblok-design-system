8c5fab147df76acd008d6d7bdd02f32f3630c928
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-10-30 13:47:45 -0300

    refactor(tag): update padding and close style icon

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -37,7 +37,7 @@ const SbTag = {
       return h(SbIcon, {
         props: {
           name: 'close',
-          size: 'small',
+          size: 'x-small',
         },
         on: {
           click: (event) => {
