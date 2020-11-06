ca41d99c124bd4d96558f971ca44f6409c8a3629
ademarCardoso <ac@storyblok.com>
2020-11-06 14:02:56 -0300

    style(editor-header): adding list of options in mobile view

--- src/components/EditorHeader/components/MobileViwer.vue
+++ src/components/EditorHeader/components/MobileViwer.vue
@@ -3,15 +3,31 @@
     <SbHeaderItem>
       <SbMenu>
         <SbMenuButton label="More" />
-
         <SbMenuList placement="bottom-start">
-          <!-- <SbMenuItem
-            v-for="option in options"
-            :key="option.id"
-            :type="option.type"
-          >
-            {{ option.name }}
-          </SbMenuItem> -->
+          <SbMenuGroup v-if="actions" title="More Actions">
+            <SbMenuItem
+              v-for="act in actions"
+              :key="act.id"
+              @click="handleSelectNewAction(act.name)"
+            >
+              <SbIcon :name="act.name" size="large" /> {{ act.name }}
+            </SbMenuItem>
+          </SbMenuGroup>
+          <SbMenuGroup v-if="languages" title="Languages">
+            <SbMenuItem
+              v-for="lang in languages"
+              :key="lang.id"
+              @click="handleSetNewLanguage(lang)"
+            >
+              {{ lang }}
+            </SbMenuItem>
+          </SbMenuGroup>
+          <SbMenuGroup v-if="users" title="People viewing">
+            <SbMenuItem v-for="user in users" :key="user.id">
+              <SbAvatar :src="user.src" size="small" :name="user.name" />
+              {{ user.name }}
+            </SbMenuItem>
+          </SbMenuGroup>
         </SbMenuList>
       </SbMenu>
     </SbHeaderItem>
@@ -20,15 +36,15 @@
 
 <script>
 import SbHeaderItem from './HeaderItem'
-// import SbAvatar from '../../Avatar'
+import SbAvatar from '../../Avatar'
 import {
   SbMenu,
   SbMenuButton,
   SbMenuList,
-  // SbMenuItem,
-  // SbMenuGroup,
+  SbMenuItem,
+  SbMenuGroup,
 } from '../../Menu'
-// import SbIcon from '../../Icon'
+import SbIcon from '../../Icon'
 
 import { sharedProps } from '../lib'
 export default {
@@ -36,17 +52,33 @@ export default {
 
   components: {
     SbHeaderItem,
-    // SbAvatar,
+    SbAvatar,
     SbMenu,
     SbMenuButton,
     SbMenuList,
-    // SbMenuItem,
-    // SbMenuGroup,
-    // SbIcon,
+    SbMenuItem,
+    SbMenuGroup,
+    SbIcon,
   },
 
   props: {
     ...sharedProps,
+  },
+
+  methods: {
+    /**
+     * method to emit new language
+     */
+    handleSetNewLanguage(lang) {
+      this.$emit('changes', { type: 'lang', language: lang })
+    },
+
+    /**
+     * method to emit new action
+     */
+    handleSelectNewAction(act) {
+      this.$emit('changes', { type: 'act', action: act })
+    },
   },
 }
 </script>
