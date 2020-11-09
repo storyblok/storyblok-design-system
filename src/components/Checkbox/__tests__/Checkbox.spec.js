1a3ee83a16ee74667decfd19dd39d0b8ca96a4bf
Gustavo Melki Portaluri <contato@gustavomelki.com.br>
2020-11-08 22:54:31 -0300

    test(checkbox): tests for checkbox component

--- src/components/Checkbox/__tests__/Checkbox.spec.js
+++ src/components/Checkbox/__tests__/Checkbox.spec.js
@@ -1,23 +1,23 @@
-import SbRadio from '..'
+import SbCheckbox from '..'
 import { mount } from '@vue/test-utils'
 
 const factory = (propsData) => {
-  return mount(SbRadio, {
+  return mount(SbCheckbox, {
     propsData,
   })
 }
 
-describe('SbRadio component', () => {
-  it('should have a input with type radio and not checked', () => {
+describe('SbCheckbox component', () => {
+  it('should have a input with type checkbox and not checked', () => {
     const wrapper = factory({
       label: 'Default',
-      nativeValue: 'default',
+      nativeValue: false,
     })
     const inputElement = wrapper.find('input')
 
-    expect(inputElement.attributes('type')).toBe('radio')
+    expect(inputElement.attributes('type')).toBe('checkbox')
     expect(inputElement.attributes('checked')).toBeUndefined()
-    expect(inputElement.attributes('value')).toBe('default')
+    expect(inputElement.attributes('value')).toBe('false')
     expect(wrapper.text()).toBe('Default')
   })
 
@@ -33,25 +33,25 @@ describe('SbRadio component', () => {
 
   it('should emit the nativeValue property when clicks on input', async () => {
     const wrapper = factory({
-      label: 'Default - Disabled',
-      nativeValue: 'Jon Doe',
+      label: 'Default',
+      nativeValue: false,
     })
 
     await wrapper.find('input').trigger('click')
 
-    expect(wrapper.emitted('input')[0]).toEqual(['Jon Doe'])
+    expect(wrapper.emitted('input')[0]).toEqual([true])
   })
 
   it('should have the outline and inline classes', async () => {
     const wrapper = factory({
-      label: 'Default - Disabled',
-      nativeValue: 'Jon Doe',
-      value: 'Jon Doe',
+      label: 'Default',
+      nativeValue: false,
+      value: false,
       inline: true,
       outline: true,
     })
 
-    expect(wrapper.classes('sb-radio--inline')).toBe(true)
-    expect(wrapper.classes('sb-radio--outline')).toBe(true)
+    expect(wrapper.classes('sb-checkbox--inline')).toBe(true)
+    expect(wrapper.classes('sb-checkbox--outline')).toBe(true)
   })
 })
