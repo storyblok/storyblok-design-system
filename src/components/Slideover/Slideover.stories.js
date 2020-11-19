a6e959616eb8573b455c667e8a49410ff9dfe06a
ademarCardoso <ac@storyblok.com>
2020-11-19 11:42:54 -0300

    refactor(slideover): adding new stories for left orientation of slideover and refactor all stories

--- src/components/Slideover/Slideover.stories.js
+++ src/components/Slideover/Slideover.stories.js
@@ -21,18 +21,20 @@ const SlideoverTemplate = (args) => ({
   }),
   template: `
     <div>
-      <SbButton label="Open Slideover!" @click="handleOpenSlide" />
+      <SbButton
+        label="Open Slideover!"
+        @click="handleOpenSlide"
+        style="margin: 0 auto; display: flex; margin-top: 30%;"/>
       
       <SbSlideover
         :is-open="show"
-        @hide="show = false">
+        @hide="show = false"
+        v-bind="{ orientation }">
         
-        <SbModalHeader title="Hi man" align="left" />
+        <SbModalHeader title="Title" align="left" />
 
         <SbModalContent style="flex: 1;">
-          <p>Storyblok helps your team to tell your story and manage 
-          content for every use-case: corporate websites, e-commerce, 
-          helpdesks, mobile apps, and screen displays.</p>
+          <p>Storyblok helps your team to tell your story and...</p>
         </SbModalContent>
 
         <SbModalFooter>
@@ -54,10 +56,30 @@ export default {
           'Slideover focus the user’s attention exclusively on one task or piece of information via a window that sits on top of the page content.',
       },
     },
+    args: {
+      orientation: 'right',
+    },
+    argTypes: {
+      orientation: {
+        name: 'orientation',
+        description:
+          'The prop `orientation` is used to define the position where the `Slideover` component will appear, the options are on the `left` or `right`, the default is on the `right`.',
+        control: {
+          type: 'select',
+          options: ['left', 'right'],
+        },
+      },
+    },
   },
 }
 
 export const Default = SlideoverTemplate.bind({})
+
+export const SlideoverOnLeft = SlideoverTemplate.bind({})
+
+SlideoverOnLeft.args = {
+  orientation: 'left',
+}
 
 export const SlideoverWithoutFooter = (args) => ({
   components: { SbSlideover, SbButton, SbModalHeader, SbModalContent },
@@ -72,10 +94,48 @@ export const SlideoverWithoutFooter = (args) => ({
   }),
   template: `
     <div>
-      <SbButton label="Open Slideover!" @click="handleOpenSlide" />
+      <SbButton
+        label="Open Slideover!"
+        @click="handleOpenSlide"
+        style="margin: 0 auto; display: flex; margin-top: 30%;"/>
+
       <SbSlideover
         :is-open="show"
-        @hide="show = false">
+        @hide="show = false"
+        v-bind="{ orientation }">
+
+        <SbModalHeader title="Hi man" align="left" />
+
+        <SbModalContent style="flex: 1;">
+          <p>Storyblok helps your team to tell your story and...</p>
+        </SbModalContent>
+      </SbSlideover>
+    </div>
+  `,
+})
+
+export const SlideoverInMaxSize = (args) => ({
+  components: { SbSlideover, SbButton, SbModalHeader, SbModalContent },
+  props: Object.keys(args),
+  methods: {
+    handleOpenSlide() {
+      this.show = true
+    },
+  },
+  data: () => ({
+    show: false,
+  }),
+  template: `
+    <div>
+      <SbButton
+        label="Open Slideover!"
+        @click="handleOpenSlide"
+        style="margin: 0 auto; display: flex; margin-top: 30%;"/>
+
+      <SbSlideover
+        :is-open="show"
+        @hide="show = false"
+        v-bind="{ orientation }">
 
         <SbModalHeader title="Hi man" align="left" />
 
