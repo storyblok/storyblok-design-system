0e18cd905fdf96267fe223729d1c2e1ce8cf47fe
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-11-19 21:05:49 -0300

    test(datepicker): fix class name

--- src/components/Datepicker/__tests__/DatepickerDays.spec.js
+++ src/components/Datepicker/__tests__/DatepickerDays.spec.js
@@ -10,7 +10,7 @@ describe('SbDatepickerDays component', () => {
     it('Should emit date for input on click', async () => {
       const internalDate = dayjs().format()
       await wrapper.setProps({ internalDate })
-      await wrapper.find('.sb-datepicker-day').trigger('click')
+      await wrapper.find('.sb-datepicker-days__item').trigger('click')
       expect(wrapper.emitted().input[0].length).toBe(1)
     })
   })
