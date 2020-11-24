4613f12d144d49e7a8d49e623a53438874353e3e
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-11-24 15:57:34 -0300

    chore: import all components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,73 @@
-import SbIcon from './Icon'
+import SbAvatar from './Avatar'
+import SbAbatarGroup from './AvatarGroup'
+import SbBadge from './Badge'
+import SbBlockUi from './BlockUI'
+import * as BreadcrumbComponents from './Breadcrumbs'
 import SbButton from './Button'
-import { SbBreadcrumbs } from './Breadcrumbs'
+import * as CardComponents from './Card'
+import SbCheckbox from './Checkbox'
+import SbDataTable from './DataTable'
+import SbDatepicker from './Datepicker'
+import SbDropArea from './DropArea'
+import * as EditorHeaderComponents from './EditorHeader'
+import SbFormItem from './FormItem'
+import SbFragment from './Fragment'
+import SbGroupButton from './GroupButton'
+import SbIcon from './Icon'
+import SbLink from './Link'
+import SbLoading from './Loading'
+import * as MenuComponents from './Menu'
+import * as MinibrowserComponents from './Minibrowser'
+import * as ModalComponents from './Modal'
+import SbNotification from './Notification'
+import SbPagination from './Pagination'
+import * as PopoverComponents from './Popover'
+import SbPortal from './Portal'
+import SbRadio from './Radio'
+import SbSelect from './Select'
+import * as SidebarComponents from './Sidebar'
+import * as SlideoverComponents from './Slideover'
+import * as SbTabPanels from './TabPanels'
+import * as SbTabs from './Tabs'
+import SbTag from './Tag'
+import SbTextField from './TextField'
+import SbTooltip from './Tooltip'
+import SbUploadDialog from './UploadDialog'
 
 export default {
+  SbAvatar,
+  SbAbatarGroup,
+  SbBadge,
+  SbBlockUi,
+  ...BreadcrumbComponents,
   SbButton,
-  SbBreadcrumbs,
+  ...CardComponents,
+  SbCheckbox,
+  SbDataTable,
+  SbDatepicker,
+  SbDropArea,
+  ...EditorHeaderComponents,
+  SbFormItem,
+  SbFragment,
+  SbGroupButton,
   SbIcon,
+  SbLink,
+  SbLoading,
+  ...MenuComponents,
+  ...MinibrowserComponents,
+  ...ModalComponents,
+  SbNotification,
+  SbPagination,
+  ...PopoverComponents,
+  SbPortal,
+  SbRadio,
+  SbSelect,
+  ...SidebarComponents,
+  ...SlideoverComponents,
+  ...SbTabPanels,
+  ...SbTabs,
+  SbTag,
+  SbTextField,
+  SbTooltip,
+  SbUploadDialog,
 }
