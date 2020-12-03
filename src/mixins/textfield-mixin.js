c2cbd7b3ab8df17eeeb9e2c3d5a294e5f1513857
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-12-03 13:08:05 -0300

    feat(textfield): order properties and add some textarea properties

--- src/mixins/textfield-mixin.js
+++ src/mixins/textfield-mixin.js
@@ -1,28 +1,11 @@
 export default {
   props: {
+    // component itself properties
     value: [String, Number],
     nativeValue: [String, Number],
-
-    type: String,
-
-    id: String,
-    disabled: Boolean,
-    required: Boolean,
-    name: String,
-    min: Number,
-    max: Number,
-    step: Number,
-    readonly: Boolean,
-    label: {
-      type: String,
-      default: null,
-    },
-    placeholder: String,
-
-    errorMessage: String,
-
-    error: Boolean,
     clearable: Boolean,
+    error: Boolean,
+    errorMessage: String,
     ghost: Boolean,
 
     iconLeft: String,
@@ -30,6 +13,37 @@ export default {
 
     prefix: String,
     suffix: String,
+    label: {
+      type: String,
+      default: null,
+    },
+
+    // HTML general properties
+    id: String,
+    disabled: Boolean,
+    name: String,
+    placeholder: String,
+    readonly: Boolean,
+    required: Boolean,
+    maxlength: [String, Number],
+    minlength: [String, Number],
+    autocomplete: String,
+
+    // HTML input properties
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    // HTML input type number
+    min: Number,
+    max: Number,
+    step: Number,
+
+    // HTML textarea properties
+    cols: Number,
+    rows: Number,
+    wrap: String,
   },
 
   data() {
