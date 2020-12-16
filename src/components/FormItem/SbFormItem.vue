c205c338fd2671deb79592d7b318871f509ec29c
Gustavo Melki Portaluri <contato@gustavomelki.com.br>
2020-12-16 20:26:46 -0300

    feature(form-item): adding helpers props to form item component

--- src/components/FormItem/SbFormItem.vue
+++ src/components/FormItem/SbFormItem.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="sb-form-item">
-    <label v-if="label" class="sb-form-item__label">{{ label }}</label>
+    <div class="sb-form-item__top-container">
+      <label v-if="label" class="sb-form-item__label">{{ label }}</label>
+      <SbTooltip v-if="helperIconText" :label="helperIconText">
+        <SbIcon
+          v-if="helperIconText"
+          color="primary-dark"
+          name="help"
+          size="small"
+          class="sb-form-item__helper-icon-text"
+        />
+      </SbTooltip>
+      <span v-if="helperText" class="sb-form-item__helper-text">{{
+        helperText
+      }}</span>
+    </div>
     <slot />
   </div>
 </template>
@@ -11,6 +25,14 @@ export default {
 
   props: {
     label: {
+      type: String,
+      default: null,
+    },
+    helperIconText: {
+      type: String,
+      default: null,
+    },
+    helperText: {
       type: String,
       default: null,
     },
