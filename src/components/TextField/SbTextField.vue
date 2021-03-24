2de8517c42c1d0874a17a25a522cff0e08f48b2b
Autocommit <it@storyblok.com>
2021-03-24 21:09:45 +0000

    autocommit

--- src/components/TextField/SbTextField.vue
+++ src/components/TextField/SbTextField.vue
@@ -12,7 +12,6 @@
         v-model="computedValue"
         v-bind="$attrs"
         class="sb-textfield__input"
-        data-testid="sb-textfield__input"
         :type="internalType"
         :placeholder="placeholder"
         :name="name"
