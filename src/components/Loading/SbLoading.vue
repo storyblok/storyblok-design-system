17c2f7365309baaf8dad641243313e8d6d105406
Autocommit <it@storyblok.com>
2021-04-14 19:38:11 +0000

    autocommit

---
+++ src/components/Loading/SbLoading.vue
@@ -0,0 +1,46 @@
+<template>
+  <Loading
+    v-if="!uiBlock"
+    v-bind="{
+      type,
+      size,
+      value,
+      showPercentage,
+      color,
+      uiBlock,
+    }"
+  />
+  <SbBlockUI v-else>
+    <Loading
+      v-bind="{
+        type,
+        size,
+        value,
+        showPercentage,
+        color,
+        uiBlock,
+      }"
+    />
+  </SbBlockUI>
+</template>
+<script>
+import SbBlockUI from '../BlockUI'
+import Loading from './components/Loading'
+import { sharedProps } from './utils'
+
+export default {
+  name: 'SbLoading',
+
+  components: {
+    Loading,
+    SbBlockUI,
+  },
+
+  props: {
+    ...sharedProps,
+  },
+}
+</script>
+<style lang="scss">
+@import './loading';
+</style>
--- src/components/Loading/utils.js
+++ src/components/Loading/utils.js
@@ -2,3 +2,28 @@
  * @type {Array<String>}
  */
 export const loadingSizes = ['small', 'normal', 'large', 'x-large']
+
+export const sharedProps = {
+  type: {
+    type: String,
+    default: 'spinner',
+  },
+  size: {
+    type: String,
+    validator: (size) => loadingSizes.indexOf(size) !== -1,
+  },
+  value: {
+    type: [Number, String],
+    default: 0,
+  },
+  showPercentage: {
+    type: Boolean,
+  },
+  color: {
+    type: String,
+    default: null,
+  },
+  uiBlock: {
+    type: Boolean,
+  },
+}
