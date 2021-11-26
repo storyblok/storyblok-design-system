cfbc3911333a0d212af4a84ab3e83ea87ebdccad
Thiago Saife Rodrigues <ts@storyblok.com>
2021-11-26 11:28:33 -0300

    feat(ds): testing imports
    - Removing imports

--- config/globals.js
+++ config/globals.js
@@ -1,10 +1,12 @@
-const path = require('path')
+// const path = require('path')
 
 const globalStyles = [
-  path.resolve(__dirname, '../src/assets/styles/variables.scss'),
-  path.resolve(__dirname, '../src/assets/styles/mixins.scss')
+  // path.resolve(__dirname, '../src/assets/styles/globals.scss'),
+  // path.resolve(__dirname, '../src/assets/styles/resets.scss'),
+  // path.resolve(__dirname, '../src/assets/styles/variables.scss'),
+  // path.resolve(__dirname, '../src/assets/styles/mixins.scss'),
 ]
 
 module.exports = {
-  globalStyles
+  globalStyles,
 }
