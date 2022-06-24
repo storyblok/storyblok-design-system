e79ef9a8f0a840bca67bdbe2955ca5ff74eaa8f1
ademarCardoso <ac@storyblok.com>
2022-06-24 14:17:26 -0300

    fix(work-393): update `deselectRow` function to remove the correct row

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -183,11 +183,10 @@ const SbDataTable = {
      * @param {Object} row
      */
     deselectRow(row) {
-      const index = this.selectedRows.indexOf(row)
-
-      if (index > -1) {
-        this.selectedRows.splice(index, 1)
-      }
+      this.selectedRows = this.selectedRows.filter((selectedRow) => {
+        if (JSON.stringify(selectedRow) !== JSON.stringify(row))
+          return selectedRow
+      })
     },
 
     /**
