67390249378947f091c7a08e3e9de0cf05c87c3b
Thiago Saife Rodrigues <ts@storyblok.com>
2022-07-01 12:17:19 -0300

    feat(int-457): improving events
    - Added events to handle previous and next events
    - Improved docs.

--- src/components/Pagination/Pagination.stories.js
+++ src/components/Pagination/Pagination.stories.js
@@ -134,11 +134,29 @@ export default {
         type: 'number',
       },
     },
-    onPerPageChange: {
-      action: 'perPageChanged',
+    perPageChange: {
+      action: 'perPageChange',
+      description: 'Event triggered when Select per page amount is changed',
+      control: {
+        type: 'select',
+        options: [25, 50, 75, 100],
+      },
     },
-    onPageChange: {
-      action: 'pageChanged',
+    pageChange: {
+      action: 'pageChange',
+      description: `Event triggered when one selects a specific page from page's list`,
+      control: {
+        type: 'select',
+        options: Array.from({ length: 10 }, (_, i) => i + 1),
+      },
+    },
+    onPreviousPage: {
+      action: 'onPreviousPage',
+      description: 'Event triggered when one presses the previous button',
+    },
+    onNextPage: {
+      action: 'onNextPage',
+      description: 'Event triggered when one presses the next button',
     },
   },
 }
--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -90,14 +90,17 @@ const SbPagination = {
     handlePreviousPage() {
       if (!this.isFirstDisabled) {
         this.updateValue(this.value - 1)
+        this.$emit('on-previous-page', this.value)
       }
     },
     handleNextPage() {
       if (!this.isLastDisabled) {
         this.updateValue(this.value + 1)
+        this.$emit('on-next-page', this.value)
       }
     },
     onPageChange(page) {
+      this.$emit('page-change', page)
       this.updateValue(page)
     },
     onPerPageChange(perPage) {
