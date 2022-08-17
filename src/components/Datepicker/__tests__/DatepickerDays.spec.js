bcb6a90c278fee9aa6e84563180dea8f3cf4e325
Fernanda Nogueira <fernanda.canto.ngr@gmail.com>
2022-08-17 09:39:13 -0300

    test(sbDatepicker): int-512 - datepickerDay

--- src/components/Datepicker/__tests__/DatepickerDays.spec.js
+++ src/components/Datepicker/__tests__/DatepickerDays.spec.js
@@ -14,4 +14,33 @@ describe('SbDatepickerDays component', () => {
       expect(wrapper.emitted().input[0].length).toBe(1)
     })
   })
+
+  describe('Test class date', () => {
+    it('Should include the `sb-datepicker-days__item--current` class in the current day', async () => {
+      const internalDate = dayjs().add(3, 'day').format()
+      await wrapper.setProps({ internalDate })
+      expect(wrapper.find('.sb-datepicker-days__item--current')).toBeTruthy()
+    })
+
+    it('Should include the `sb-datepicker-days__item--active` class in the active day', async () => {
+      const internalDate = dayjs().format()
+      await wrapper.setProps({ internalDate })
+      expect(wrapper.find('.sb-datepicker-days__item--active')).toBeTruthy()
+    })
+
+    it('Should include the `sb-datepicker-days item--disabled` class when maxDate or minDate is passed', async () => {
+      const minDate =   dayjs().add(3, 'day').format()
+      const maxDate =  dayjs().subtract(3, 'day').format()
+      await wrapper.setProps({ minDate, maxDate })
+
+      expect(wrapper.find('.sb-datepicker-days__item--disabled')).toBeTruthy()
+    })
+
+    it('Should include the `sb-datepicker-days__item--disabled` class when disabledPast is true', async () => {
+      const disabledPast =  true
+      await wrapper.setProps({ disabledPast })
+
+      expect(wrapper.find('.sb-datepicker-days__item--disabled')).toBeTruthy()
+    })
+  })
 })
