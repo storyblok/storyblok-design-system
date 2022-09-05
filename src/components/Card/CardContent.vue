8580910af15dda2d4d1e42d40914ce1c852c41b6
Lisi Linhart <ewl@storyblok.com>
2022-09-05 16:53:51 +0200

    refactor: sb card

---
+++ src/components/Card/CardContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sb-card__content">
+    <slot></slot>
+    <div v-if="isLoading" class="sb-card__loading">
+      <SbLoading size="x-large" color="primary" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SbLoading from '../Loading'
+export default {
+  name: 'SbCardContent',
+  components: {
+    SbLoading,
+  },
+  inject: ['cardContext'],
+  computed: {
+    context() {
+      return this.cardContext()
+    },
+    isLoading() {
+      return this.context.isLoading
+    },
+  },
+}
+</script>
