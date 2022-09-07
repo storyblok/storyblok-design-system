31fe4db8f9a6e2c04e3ee9e934e664f4b5ed9893
Lisi Linhart <ewl@storyblok.com>
2022-09-07 11:22:08 +0200

    refactor: datatable components

---
+++ src/components/DataTable/components/SbDataTableHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <tr>
+    <th v-if="allowSelection" class="sb-data-table__head-cell">
+      <SbCheckbox
+        v-if="selectionMode === 'multiple'"
+        :indeterminate="isIndeterminate"
+        :value="allRowsSelected"
+        @click.native="handleAllRowsSelected"
+      />
+    </th>
+    <SbDataTableHeaderCell
+      v-for="(elem, index) in headers"
+      :key="index"
+      :column="elem"
+      :sorted-key="sortedKey"
+    />
+  </tr>
+</template>
+
+<script>
+import SbCheckbox from '../../Checkbox'
+import SbDataTableHeaderCell from './SbDataTableHeaderCell'
+import sharedProps from '../sharedProps'
+
+export default {
+  name: 'SbDataTableHeader',
+
+  components: {
+    SbCheckbox,
+    SbDataTableHeaderCell,
+  },
+
+  inject: ['dataTableContext'],
+
+  props: {
+    allowSelection: sharedProps.allowSelection,
+    headers: sharedProps.headers,
+    selectedRows: sharedProps.selectedRows,
+    selectionMode: sharedProps.selectionMode,
+    sortedKey: {
+      type: String,
+      default: null,
+    },
+    allRowsSelected: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    context() {
+      return this.dataTableContext()
+    },
+
+    isIndeterminate() {
+      return !this.allRowsSelected && !!this.selectedRowsLength
+    },
+
+    selectedRowsLength() {
+      return this.selectedRows.length
+    },
+  },
+
+  methods: {
+    handleAllRowsSelected() {
+      this.allRowsSelected
+        ? this.context.deselectAll()
+        : this.context.selectAll()
+    },
+  },
+}
+</script>
