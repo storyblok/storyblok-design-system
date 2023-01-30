91f1009f29db1b7324e90f860db4550198a48360
Lisi Linhart <ewl@storyblok.com>
2023-01-30 10:31:05 +0100

    chore: initial responsive setup datatable

--- src/components/Popover/index.js
+++ src/components/Popover/index.js
@@ -3,5 +3,23 @@ import './popover.scss'
 import SbPopover from './index.vue'
 import SbPopoverArrow from './PopoverArrow.vue'
 
+export const placementOptions = [
+  'auto',
+  'auto-start',
+  'auto-end',
+  'top',
+  'top-start',
+  'top-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+  'right',
+  'right-start',
+  'right-end',
+  'left',
+  'left-start',
+  'left-end',
+]
+
 export { SbPopover, SbPopoverArrow }
 export default SbPopover
