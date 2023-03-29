8a86ae4a3829855c9570d013f03796eada6cf730
Lisi Linhart <ewl@storyblok.com>
2023-03-29 11:08:07 +0200

    feat: add viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,57 @@ import './preview.css'
 import BlokInkPlugin from '../src/main'
 import { setup } from '@storybook/vue3';
 
+const customViewports = {
+  xxs: {
+    name: 'XXS',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+  },
+  xs: {
+    name: 'XS',
+    styles: {
+      width: '480px',
+      height: '800px',
+    },
+  },
+  sm: {
+    name: 'SM',
+    styles: {
+      width: '640px',
+      height: '960px',
+    },
+  },
+  md: {
+    name: 'MD',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  lg: {
+    name: 'LG',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    },
+  },
+  xl: {
+    name: 'XL',
+    styles: {
+      width: '1280px',
+      height: '800px',
+    },
+  },
+  '2xl': {
+    name: '2XL',
+    styles: {
+      width: '1536px',
+      height: '864px',
+    },
+  },
+}
 setup((app) => {
   app.use(BlokInkPlugin)
 })
@@ -16,5 +67,8 @@ export const parameters = {
       order: ['Introduction', 'Design', 'Basic', 'Forms', 'Navigation', 'Data', 'Interface'],
       locales: '',
     },
+  },
+  viewport: {
+    viewports: customViewports,
   },
 };
