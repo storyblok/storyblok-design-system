c2abc143438d6c738d511be507552d6dbab871ae
Lisi Linhart <ewl@storyblok.com>
2023-05-04 11:17:41 +0200

    feat: try without variables

--- stories/Design/Spacing/Spacing.stories.js
+++ stories/Design/Spacing/Spacing.stories.js
@@ -9,18 +9,6 @@ export const AllSpacing = () => ({
   template: `
   <section>
     <div>
-      <h1 class="my-5">Variables</h1>
-      <pre><code>
-  s-1: 4px
-  s-2: 8px
-  s-3: 12px
-  s-4: 16px
-  s-5: 24px
-  s-6: 32px
-  s-7: 40px
-  s-8: 48px
-      </code></pre>
-
       <h1 class="my-5">Margin</h1>
       <div class="wrapper">
         <div class="outer-margin"><div class="square m-1">m-1</div></div>
