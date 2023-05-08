f241f09a4400d5b83c258b9bfb71705d2a646e6c
Lisi Linhart <ewl@storyblok.com>
2023-05-08 09:51:34 +0200

    refactor: naming of spacing helpers

--- stories/Design/Spacing/Spacing.stories.js
+++ stories/Design/Spacing/Spacing.stories.js
@@ -22,27 +22,27 @@ export const AllSpacing = () => ({
 
       <h1 class="my-5">Margin</h1>
       <div class="wrapper">
-        <div class="outer-margin"><div class="square m-1">m-1</div></div>
-        <div class="outer-margin"><div class="square mt-2">mt-2</div></div>
-        <div class="outer-margin"><div class="square mr-3">mr-3</div></div>
-        <div class="outer-margin"><div class="square mb-4">mb-4</div></div>
-        <div class="outer-margin"><div class="square ml-5">ml-5</div></div>
-        <div class="outer-margin"><div class="square mx-6">mx-6</div></div>
-        <div class="outer-margin"><div class="square my-7">my-7</div></div>
-        <div class="outer-margin"><div class="square m-8">m-8</div></div>
+        <div class="outer-margin"><div class="square sb-m-1">sb-m-1</div></div>
+        <div class="outer-margin"><div class="square sb-mt-2">sb-mt-2</div></div>
+        <div class="outer-margin"><div class="square sb-mr-3">sb-mr-3</div></div>
+        <div class="outer-margin"><div class="square sb-mb-4">sb-mb-4</div></div>
+        <div class="outer-margin"><div class="square sb-ml-5">sb-ml-5</div></div>
+        <div class="outer-margin"><div class="square sb-mx-6">sb-mx-6</div></div>
+        <div class="outer-margin"><div class="square sb-my-7">sb-my-7</div></div>
+        <div class="outer-margin"><div class="square sb-m-8">sb-m-8</div></div>
       </div>
 
-      <h1 class="my-5">Padding</h1>
+      <h1 class="my-5">sb-Padding</h1>
 
       <div class="wrapper">
-        <div class="outer-padding"><div class="square p-1">p-1</div></div>
-        <div class="outer-padding"><div class="square pt-2">pt-2</div></div>
-        <div class="outer-padding"><div class="square pr-3">pr-3</div></div>
-        <div class="outer-padding"><div class="square pb-4">pb-4</div></div>
-        <div class="outer-padding"><div class="square pl-5">pl-5</div></div>
-        <div class="outer-padding"><div class="square px-6">px-6</div></div>
-        <div class="outer-padding"><div class="square py-7">py-7</div></div>
-        <div class="outer-padding"><div class="square p-8">p-8</div></div>
+        <div class="outer-padding"><div class="square sb-p-1">sb-p-1</div></div>
+        <div class="outer-padding"><div class="square sb-pt-2">sb-pt-2</div></div>
+        <div class="outer-padding"><div class="square sb-pr-3">sb-pr-3</div></div>
+        <div class="outer-padding"><div class="square sb-pb-4">sb-pb-4</div></div>
+        <div class="outer-padding"><div class="square sb-pl-5">sb-pl-5</div></div>
+        <div class="outer-padding"><div class="square sb-px-6">sb-px-6</div></div>
+        <div class="outer-padding"><div class="square sb-py-7">sb-py-7</div></div>
+        <div class="outer-padding"><div class="square sb-p-8">sb-p-8</div></div>
       </div>
     </div>
   </section>
