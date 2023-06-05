5bfc737ffc177147f3dc0f4686d9b64ad26d21af
Lisi Linhart <ewl@storyblok.com>
2023-06-05 11:12:13 +0200

    chore: remove old dependency

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,5 +1,4 @@
 import { directive as ClickOutside } from 'click-outside-vue3'
 import Tooltip from './tooltip'
-import InfiniteScroll from './infinite-scroll'
 
-export { ClickOutside, Tooltip, InfiniteScroll }
+export { ClickOutside, Tooltip }
