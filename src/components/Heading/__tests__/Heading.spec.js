eda1e53924b85f3cae2774692879911192bf6c60
ECJ222 <enochchejieh@gmail.com>
2023-07-21 21:37:47 +0100

    chore: update tests

--- src/components/Heading/__tests__/Heading.spec.js
+++ src/components/Heading/__tests__/Heading.spec.js
@@ -2,9 +2,9 @@ import { mount } from '@vue/test-utils'
 
 import SbHeading from '..'
 
-const factory = (props) => {
+const factory = (options) => {
   return mount(SbHeading, {
-    props,
+    ...options,
   })
 }
 
@@ -12,18 +12,70 @@ describe('Test SbHeading Component', () => {
   it('should render the default h1 tag', () => {
     const wrapper = factory()
 
-    expect(wrapper.find('h1')).toBeTruthy()
+    expect(wrapper.find('h1').exists()).toBeTruthy()
   })
 
-  it.todo('should render different tag elements depends on the as prop')
+  describe('when a tag name is passed to the `as` prop', () => {
+    it('should render tag element', () => {
+      const options = {
+        props: {
+          as: 'h2',
+        },
+      }
+      const wrapper = factory(options)
 
-  it.todo(
-    'should render different font-weight classes depends on the font-weight prop'
-  )
+      expect(wrapper.find('h2').exists()).toBeTruthy()
+    })
+  })
 
-  it.todo(
-    'should render different font-size style depends on the customFontSize prop'
-  )
+  describe('when `font-weight` prop is passed', () => {
+    it('should render font-weight class correctly', () => {
+      const options = {
+        props: {
+          fontWeight: 'medium',
+        },
+      }
+      const wrapper = factory(options)
 
-  it.todo('should render the expected text in the default slot')
+      expect(wrapper.find('h1').classes()[1]).toBe('font-medium')
+    })
+  })
+
+  describe('when `customFontSize` props is passed', () => {
+    it('should render font-size style correctly', () => {
+      const options = {
+        props: {
+          customFontSize: '14px',
+        },
+      }
+      const wrapper = factory(options)
+
+      expect(wrapper.attributes().style).toBe('font-size: 14px;')
+    })
+  })
+
+  describe('when node is passed into default slot', () => {
+    it('should render text node correctly', () => {
+      const options = {
+        slots: {
+          default: 'Main Content',
+        },
+      }
+      const wrapper = factory(options)
+
+      expect(wrapper.text()).toMatch(/Main Content/i)
+    })
+    it('should render element node correctly', () => {
+      const options = {
+        slots: {
+          default: '<div class="header-title">Main Content</div>',
+        },
+      }
+      const wrapper = factory(options)
+
+      expect(wrapper.find('.header-title').html()).toBe(
+        '<div class="header-title">Main Content</div>'
+      )
+    })
+  })
 })
