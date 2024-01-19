c0783d9ac3d2b1e6eec826e2feb9b24f038c7e6e
Lisi Linhart <ewl@storyblok.com>
2024-01-19 14:34:29 +0100

    poc: introduce PV tab menu

---
+++ src/components/TabMenu/SbTabMenu.stories.ts
@@ -0,0 +1,48 @@
+import SbTabMenu from './index'
+import type { Args, Meta, StoryObj } from '@storybook/vue3'
+import type { MenuItem } from 'primevue/menuitem'
+
+type Story = StoryObj<typeof SbTabMenu>
+
+const defaultItems = [
+  {
+    label: 'Option 1',
+  },
+  {
+    label: 'Option 2',
+  },
+  {
+    label: 'Option Disabled',
+    disabled: true,
+  },
+  {
+    label: 'Option Invisible',
+    visible: false,
+  },
+  {
+    label: 'Customized Option',
+    class: 'sb-custom-class',
+  },
+] as MenuItem[]
+
+const meta: Meta<typeof SbTabMenu> = {
+  title: 'Navigation/SbTabMenu',
+  component: SbTabMenu,
+  args: {
+    model: defaultItems,
+  },
+}
+
+const renderFn = (args: Args): unknown => ({
+  components: { SbTabMenu },
+  setup(): Args {
+    return { args }
+  },
+  template: `<SbTabMenu v-bind="args" />`,
+})
+
+export const Default: Story = {
+  render: renderFn,
+}
+
+export default meta
---
+++ src/components/TabMenu/SbTabMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <PrimeTabMenu
+    :pt="{
+      root: 'sb-tab-menu',
+      menu: 'sb-tab-menu__menu',
+      menuitem: 'sb-tab-menu__menu-item',
+      action: 'sb-tab-menu__action',
+      icon: 'sb-tab-menu__icon',
+      label: 'sb-tab-menu__label',
+      inkbar: 'sb-tab-menu__inkbar',
+      hooks: 'sb-tab-menu__hooks',
+      ...$props.pt,
+    }"
+    @tab-change="(event) => $emit('update:modelValue', event)"
+  />
+</template>
+
+<script setup>
+import PrimeTabMenu from 'primevue/tabmenu'
+
+defineProps({
+  ...PrimeTabMenu.props,
+})
+
+defineEmits(['update:modelValue', ...PrimeTabMenu.emits])
+</script>
+
+<style lang="scss" src="./tab-menu.scss" />
